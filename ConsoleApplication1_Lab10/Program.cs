669b11106b4baa62a3bd4d6ec45d097c8c9ec2ea
Crystal LeCoy <crystallecoy@gmail.com>
2016-11-28 15:29:28 -0500

    Clean up console app. Omit use of Movie List on line 27.

--- ConsoleApplication1_Lab10/Program.cs
+++ ConsoleApplication1_Lab10/Program.cs
@@ -10,25 +10,21 @@ namespace ConsoleApplication1_Lab10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Movie List Application!");
-            Console.WriteLine(Environment.NewLine);
-            Console.WriteLine("There are 100 movies in this list.");
+            Console.WriteLine("Welcome to the Movie List Application!" + Environment.NewLine);
+            //Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("There are 100 movies in this list." + Environment.NewLine);
 
             bool again = true;
             do
             {
-                Console.WriteLine("What category are you interested in?");
-                Console.WriteLine("The catagories are animated, drama, horror, and scifi.");
-                //string[] Categories = new string[] { "animated", "drama", "horror", "scifi" };
-                //List<string> CategoryList = new List<string>(Categories);
-                //string text = string.Join(", ", Categories);
-                //Console.WriteLine($"The categories are {text}.");
+                Console.WriteLine("What category are you interested in? (animated, drama, horror, or scifi)");
+                //Console.WriteLine("The catagories are: animated, drama, horror, and scifi." + Environment.NewLine);
                 string userInput = Console.ReadLine();
-                Console.WriteLine(Environment.NewLine);
 
                 List<Movie> MovieList = new List<Movie>();
                 MovieList = MovieIO.MovieCatalog;
-                MovieList = MovieList.Where(mbox => mbox.Title == Title).OrderBy (mbox => mbox.Title).ToList();
+
+                //MovieList = MovieList.Where(mbox => mbox.Title == userInput).OrderBy (mbox => mbox.Title).ToList();
 
                 int i = 0;
                 foreach (Movie movie in MovieList)
@@ -41,7 +37,7 @@ namespace ConsoleApplication1_Lab10
 
 
                 Console.WriteLine(Environment.NewLine);
-                Console.WriteLine("Would you like to continue? Yes or No");
+                Console.WriteLine("Would you like to continue? (Yes or No)");
                 string answer = Console.ReadLine();
                 again = answer.ToLower() == "yes" || answer.ToLower() == "y";
             }
